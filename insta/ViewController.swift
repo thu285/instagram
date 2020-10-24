7bea049ed28260c304bb7a0da095ba4b4f00d888
thu285 <thu.hoaido.285@gmail.com>
2020-10-23 20:55:47 -0400

    Initial Commit

---
+++ insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  insta
+//
+//  Created by Thu Do on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
