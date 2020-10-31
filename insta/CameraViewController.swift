b0eea63d458fd6b924db608185027f4f5149ecce
thu285 <thu.hoaido.285@gmail.com>
2020-10-31 18:57:55 -0400

    add cancel button in post image view

--- insta/CameraViewController.swift
+++ insta/CameraViewController.swift
@@ -55,7 +55,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        let scaledImage = image.af.imageAspectScaled(toFill: size)
         imageView.image = scaledImage
         dismiss(animated: true, completion: nil)
     }
@@ -64,6 +64,9 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         view.endEditing(true)
     }
     
+    @IBAction func onBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
